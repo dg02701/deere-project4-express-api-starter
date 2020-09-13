3fcc434a1d07b5560cb7b1be170fbf03990a4610
Darryl Grissom <degman@mchsi.com>
2020-09-13 17:08:39 -0500

    working on seeders and updated server homepage msg

---
+++ seeders/20200913214334-demo-cards.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "Cards",
+      [
+        {
+          deckId: 1,
+          prompt: "",
+          question: "4 + 5 = ?",
+          answer: "9",
+          source: "",
+        },        {
+          deckId: 2,
+          prompt: "Who said?",
+          question: "Die Hard is a Christmas movie",
+          answer: "Everyone",
+          source: "the interwebs, so it is true",
+        },
+        {
+          deckId: 1,
+          prompt: "",
+          question: "4^2 = ?",
+          answer: "16",
+          source: "",
+        },
+        {
+          deckId: 2,
+          prompt: "Who said?",
+          question: "They're gonna need some more FBI guys, I guess.",
+          answer: "Deputy Chief Dwayne T. Robinson",
+          source: "",
+        },
+        {
+          deckId: 1,
+          prompt: "",
+          question: "3 + 2 = ?",
+          answer: "5",
+          source: "",
+        },
+        {
+          deckId: 1,
+          prompt: "",
+          question: "10 + 11 = ?",
+          answer: "21",
+          source: "",
+        },
+      ]
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
