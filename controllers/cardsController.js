e9f53df113d4e23704ce7178ad734ff6b95fe08e
Darryl Grissom <degman@mchsi.com>
2020-09-14 13:41:20 -0500

    added cardsCntrl file and GET ALL CARDS route

---
+++ controllers/cardsController.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const CardModel = require("../models").Card;
+const DeckModel = require("../models").Deck;
+
+//GET ALL CARDS
+//GET http://localhost:3000/api/cards
+router.get("/", async(req, res) => {
+    let cards = await CardModel.findAll();
+    res.json({ cards });
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.get("/", (req, res) => {
 app.use("/api/auth", require("./controllers/authController.js"));
 app.use("/api/users", require("./controllers/usersController.js"));
 app.use("/api/decks", require("./controllers/decksController.js"));
+app.use("/api/cards", require("./controllers/cardsController.js"));
 
 app.listen(process.env.PORT, () => {
   console.log("Nodemon listening, Darryl");
